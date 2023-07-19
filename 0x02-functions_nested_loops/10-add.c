3e4c59f663b81bab492601fa39ea3630b8fda80f
Ismail <f14vsf22raptor@gmail.com>
2023-07-19 13:45:46 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add 2 values
+ *@i: the first number to be computed
+ *@j: the second number
+ *Return: integer value
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
