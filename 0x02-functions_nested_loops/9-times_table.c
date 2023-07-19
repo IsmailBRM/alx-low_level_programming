50c7d2e2ecdbb8f8abe06c9cccd9797e78b38571
Ismail <f14vsf22raptor@gmail.com>
2023-07-19 08:49:54 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,8 +23,8 @@ void times_table(void)
 			{
 				_putchar((Res / 10) + '0');
 				_putchar((Res % 10) + '0');
+				_putchar(',');
 			}
-			_putchar(',');
 			_putchar(' ');
 		}
 		_putchar('\n');
