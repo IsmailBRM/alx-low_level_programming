e633c740b5857b344d726ccbcd81c9874b9428af
Ismail <f14vsf22raptor@gmail.com>
2023-07-19 04:06:43 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+}
