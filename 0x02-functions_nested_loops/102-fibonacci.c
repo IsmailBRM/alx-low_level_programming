38a074368371473c4414f6be1dfa4ae911c8bda6
Ismail <f14vsf22raptor@gmail.com>
2023-07-20 05:45:18 -0700

    task14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int cnt;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; cnt < 50; cnt++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (cnt == 49)
+			printf("\n");
+		else
+			printf(",");
+	}
+	return (0);
+}
