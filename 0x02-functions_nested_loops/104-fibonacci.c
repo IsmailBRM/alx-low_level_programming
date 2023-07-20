03b3bc2c186cbc4b30cd77f1cf032da99db998e1
Ismail <f14vsf22raptor@gmail.com>
2023-07-20 07:01:26 -0700

    task16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * numLength - return the length of string
+ *
+ *@num: operand number
+ *
+ *Return: nember of digits
+ */
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0
