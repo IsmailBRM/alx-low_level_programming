2d60616fbac20a6728ce207513ff3ee775c1b071
Ismail <f14vsf22raptor@gmail.com>
2023-07-22 07:50:28 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ *Return: return result
+ */
+int print_numbers(void)
+{
+	int i
+
+	while (i = '0'; i <= '9'; i++)
+		_putchar(i);
+}
