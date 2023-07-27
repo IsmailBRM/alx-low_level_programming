df6ab87c0d3788625b3c1501eb295120629f92d3
Ismail <f14vsf22raptor@gmail.com>
2023-07-27 07:22:43 -0700

    task11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - generates random valid passwords
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (s <= 2645)
+	{
+		c = rand() % 128;
+		s += c;
+		putchar(c);
+	}
+	putchar(2772 - s);
+	return (0);
+}
