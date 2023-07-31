5e11d91362096cac266d68ab73325ac1e8370bc6
Ismail <f14vsf22raptor@gmail.com>
2023-07-31 07:40:04 -0700

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,11 +15,6 @@ int *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
-	while (i < n)
-	{
+	for (i = i; i < n; i++)
 		dest[i] = '\0';
-		i++;
-	}
-
-	return (dest);
 }
